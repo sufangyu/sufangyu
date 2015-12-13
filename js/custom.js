0cb0d2d7028c3db2c0038e4fb7dfa34d2f9e78b6
方雨_Yu <304683191@qq.com>
2015-12-13 23:16:47 +0800

    正式版
    增加广告轮换布局

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+require([], function (){
+	//owl
+	$(document).ready(function() {
+		$(".owl-demo").owlCarousel({
+			autoPlay   : true,
+			stopOnHover: true,
+			stopOnHover: true,
+			navigation : false,
+			slideSpeed : 300,
+			paginationSpeed : 400,
+			singleItem : true			
+		});
+	});
+	
+	
+});
